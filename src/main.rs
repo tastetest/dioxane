ebfe460689b3e7083a5a5c5f3eaca0ce360a2f14
tastetest <tastytothesupreme@gmail.com>
2023-04-23 18:26:17 -0600

    moved code, added a bunch of new stuff

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
-mod pipeline;
+use learn_vulkan::run;
 
 fn main() {
-    let event_loop = EventLoop::new();
-    let window = winit::window::Window::new(&event_loop).unwrap();
-    pollster::block_on(pipeline::pipeline::run(event_loop, window));
+    pollster::block_on(run());
 }
